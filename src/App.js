953454a5a2b2615faa73880207e201bf6d1e7db1
gedavarol <gizemeda91@gmail.com>
2021-09-28 14:53:47 +0200

    Functioned search engine part

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Hello </div>;
+  return (
+    <div className="container">
+      <div className="App">Hello</div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center">
+        <small>Coded by Gizem Eda Varol</small>
+      </footer>
+    </div>
+  );
 }
