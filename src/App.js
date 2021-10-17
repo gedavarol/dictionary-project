383dc2f5d214c97da02a9ad945a8058b3e6f3094
gedavarol <gizemeda91@gmail.com>
2021-10-17 18:47:35 +0200

    Updated open-sourced on footer

--- src/App.js
+++ src/App.js
@@ -10,13 +10,29 @@ export default function App() {
       </main>
       <footer>
         <small>
-          Coded by{" "}
+          This project was coded by{" "}
           <a
             href="https://portfolio-gizem-eda-varol.netlify.app/about.html"
             target="_blank"
             rel="noreferrer"
           >
             Gizem Eda Varol
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/gedavarol/dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          and{" "}
+          <a
+            href="https://dictionary-app-gizem.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify{" "}
           </a>
         </small>
       </footer>
